e2e670134192ce1a6849addb458f13a8001fb307
Westley Knight <westley.knight@gmail.com>
2021-07-05 09:36:14 +0100

    Setup v3 of CO Review LPA Questionnaire

---
+++ app/routes/case-officer-lpaq-review/v3.js
@@ -0,0 +1,12 @@
+module.exports = function (router) {
+
+  var v = "v3";
+  var base = "/case-officer-lpaq-review/"+v+"/";
+
+  // Cross-check answers against the PO report
+/*
+    router.post(base+'review-questionnaire', function (req, res) {
+      res.redirect(base+'review-questionnaire-check');
+    })
+*/
+}
