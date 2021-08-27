a19bd7b67ecbc19b2c8bc38aaf903c1ff8e60116
Westley Knight <westley.knight@gmail.com>
2021-08-27 17:25:13 +0100

    Start of appeals list page for inspectors

---
+++ app/routes/inspector/v4.js
@@ -0,0 +1,12 @@
+module.exports = function (router) {
+
+  var v = "v4";
+  var base = "/inspector/"+v+"/";
+
+  // 
+/*
+    router.post(base+'review-questionnaire/review', function (req, res) {
+      res.redirect(base+'review-questionnaire/check');
+    })
+*/
+}
