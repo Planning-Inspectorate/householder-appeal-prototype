a5328363f4beb760149b550a9e9d8e5d4b63a02f
Westley Knight <westley.knight@gmail.com>
2021-10-20 11:13:05 +0100

    Full and householder eligibility completed
    Rather than 'eligibility' we should refer to this as 'Before you start'

--- app/routes/eligibility/v9.js
+++ app/routes/eligibility/v9.js
@@ -3,6 +3,13 @@ module.exports = function (router) {
   var v = "v9";
   var base = "/eligibility/"+v+"/";
 
+  /*
+   * All routing in theis file is based on the logic in thes Miro board frame:
+   * https://miro.com/app/board/o9J_lv1IHjc=/?moveToWidget=3074457365971757787&cot=14
+   * 
+   * The relevant frame is titled "v9a Before you start (eligibility checker)"
+   */
+
   // Question 1
     router.post(base+'planning-department', function (req, res) {
       if (
@@ -24,12 +31,12 @@ module.exports = function (router) {
   router.post(base+'what-are-you-appealing', function (req, res) {
     if (req.session.data["eligibility-"+v+"-whatareyouappealing"] == "I have not made a planning application") {
       res.redirect(base+'shutter/planning-application-not-made');
-    } else if (req.session.data["eligibility-"+v+"-whatareyouappealing"] == "A different type") {
-      res.redirect(base+'shutter/planning-application-different-type');
+    } else if (req.session.data["eligibility-"+v+"-whatareyouappealing"] == "Something else") {
+      res.redirect(base+'shutter/planning-application-something-else');
     } else if (req.session.data["eligibility-"+v+"-whatareyouappealing"] == "Householder planning") {
       res.redirect(base+'listed-building');
     } else {
-      res.redirect(base+'appeal-relates-to');        
+      res.redirect(base+'appeal-about');        
     }
   })
 
@@ -53,30 +60,33 @@ module.exports = function (router) {
     if (req.session.data["eligibility-"+v+"-permissiongrantedrefused"] == "I have not received a decision"){
       res.redirect(base+'decision-date-due');
     } else {
-      if (req.session.data["eligibility-"+v+"-whatareyouappealing"] == "Full planning"){
-        res.redirect(base+'notice-decision-date-full');
-      } else {
-        if (req.session.data["eligibility-"+v+"-permissiongrantedrefused"] == "Granted"){
-          res.redirect(base+'notice-decision-date-full');
-        } else {
-          res.redirect(base+'notice-decision-date-householder');
-        }
-      }      
+      res.redirect(base+'notice-decision-date');
     }
   })
 
-  router.post(base+'notice-decision-date-householder', function (req, res) {
+  router.post(base+'notice-decision-date', function (req, res) {
 
     // get decision due date from user
-    var enteredDay = req.session.data['eligibility-'+v+'-decisiondatehouseholder-day'];
-    var enteredMonth = req.session.data['eligibility-'+v+'-decisiondatehouseholder-month'];
-    var enteredYear = req.session.data['eligibility-'+v+'-decisiondatehouseholder-year'];
+    var enteredDay = req.session.data['eligibility-'+v+'-decisiondate-day'];
+    var enteredMonth = req.session.data['eligibility-'+v+'-decisiondate-month'];
+    var enteredYear = req.session.data['eligibility-'+v+'-decisiondate-year'];
     var enteredDate = new Date(enteredYear, enteredMonth - 1, enteredDay); 
 
     // calculate deadline for appeal based on decision due date
-    // 12 weeks for the HAS appeal 
     var deadlineDate = new Date(enteredDate);
-    deadlineDate.setDate(deadlineDate.getDate() + (12*7));
+    if (
+      (req.session.data['eligibility-'+v+'-whatareyouappealing'] == "Full planning") ||
+      (
+        (req.session.data['eligibility-'+v+'-whatareyouappealing'] == "Householder planning") &&
+        (req.session.data['eligibility-'+v+'-permissiongrantedrefused'] == "Granted")
+      )
+    ){
+      // 6 months = 26 weeks for full appeals
+      deadlineDate.setDate(deadlineDate.getDate() + (26*7));
+    } else {
+      // 12 weeks for the HAS appeals
+      deadlineDate.setDate(deadlineDate.getDate() + (12*7));
+    }
 
     req.session.data["eligibility-"+v+"-deadline-day"] = deadlineDate.getDate();
     req.session.data["eligibility-"+v+"-deadline-month"] = deadlineDate.getMonth() + 1;
@@ -85,41 +95,44 @@ module.exports = function (router) {
     var todaysDate = new Date();
 
     if (todaysDate > deadlineDate) {
-      res.redirect(base+'shutter/out-of-time-householder')
-    } else {
-      // if householder then... Is your appeal about a listed building?
-      if (req.session.data["eligibility-"+v+"-whatareyouappealing"] == "Householder planning"){
-        res.redirect(base+'claiming-costs');
+      if (req.session.data['eligibility-'+v+'-whatareyouappealing'] == "Full planning"){
+        res.redirect(base+'shutter/out-of-time-full')
+      } else if (
+        req.session.data['eligibility-'+v+'-whatareyouappealing'] == "Householder planning" &&
+        req.session.data['eligibility-'+v+'-permissiongrantedrefused'] == "Granted"
+      ) {
+        res.redirect(base+'shutter/out-of-time-full')
+      } else {
+        res.redirect(base+'shutter/out-of-time-householder')
       }
-      // if full then... Does your appeal relate to any of the following? Major dwellings *salute*
+    } else {
+      res.redirect(base+'claiming-costs');
     }
   })
 
   router.post(base+'claiming-costs', function (req, res) {
     if (req.session.data["eligibility-"+v+"-claimingcosts"] == "No"){
-      res.redirect(base+'task-list');
+      if (req.session.data['eligibility-'+v+'-whatareyouappealing'] == "Full planning"){
+        res.redirect(base+'appeal-procedure');
+      } else if (req.session.data['eligibility-'+v+'-whatareyouappealing'] == "Householder planning") {
+        if (req.session.data['eligibility-'+v+'-permissiongrantedrefused'] == "Granted"){
+          res.redirect(base+'appeal-procedure');
+        } else {
+          res.redirect(base+'task-list');
+        }
+      }
     } else {
       res.redirect(base+'shutter/claiming-costs');
     }
   })
-  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  router.post(base+'appeal-about', function (req, res) {
+    if (req.session.data["eligibility-"+v+"-appealabout"].includes("None of these") ){
+      res.redirect(base+'enforcement-notice');
+    } else {
+      res.redirect(base+'shutter/appeal-about');
+    }
+  })
 
   router.post(base+'decision-date-due', function (req, res) {
 
@@ -143,8 +156,15 @@ module.exports = function (router) {
     if (todaysDate > deadlineDate) {
       res.redirect(base+'shutter/out-of-time-no-decision')
     } else {
-      // if householder then... Is your appeal about a listed building?
-      // if full then... Does your appeal relate to any of the following? Major dwellings *salute*
+      res.redirect(base+'claiming-costs')
+    }
+  })
+
+  router.post(base+'appeal-procedure', function (req, res) {
+    if (req.session.data["eligibility-"+v+"-appealprocedure"] == "Written representations"){
+      res.redirect(base+'task-list');
+    } else {
+      res.redirect(base+'shutter/appeal-procedure');
     }
   })
 
