afad53295abf0727344f68bac8219bc307bdd712
Westley Knight <westley.knight@gmail.com>
2021-10-22 13:50:07 +0100

    Edit appeals - all editable sections now dynamic

--- app/routes/cst/v5a.js
+++ app/routes/cst/v5a.js
@@ -86,4 +86,33 @@ module.exports = function (router) {
 
   })
 
+  router.post(base+'edit/representations-add', function (req, res) {
+
+    // collate all variables from form
+    var newRepresentation = {
+      'file': req.session.data['cst-'+v+'-representations-file'],
+      'withdrawn': false
+    }
+  
+    // delete form data so revisiting the upload page doesn't show stored info from last upload
+    req.session.data['cst-'+v+'-representations-file'] = null
+
+    // if array doesn't exist, create it
+    if (!req.session.data['cst-'+v+'-files-representations']) {
+      req.session.data['cst-'+v+'-files-representations'] = []
+    }
+
+    // Add uploaded file info to array
+    req.session.data['cst-'+v+'-files-representations'].push(newRepresentation)
+
+    // redirect to list of uploaded files
+    res.redirect(base+'edit/representations');
+    
+  })
+
+  router.post(base+'edit/representations-withdraw', function (req, res) {
+    req.session.data['cst-'+v+'-files-representations'][req.session.data['row']-1]['withdrawn'] = true;
+    res.redirect(base+'edit/representations');    
+  })
+
 }
