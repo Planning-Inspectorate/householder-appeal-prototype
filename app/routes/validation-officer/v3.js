93b57d3e5e5ad112f493d64b157f3129c3eacfd5
Westley Knight <westley.knight@gmail.com>
2021-11-03 16:28:24 +0000

    Change outcome and incomplete reasons added

--- app/routes/validation-officer/v3.js
+++ app/routes/validation-officer/v3.js
@@ -52,6 +52,29 @@ module.exports = function (router) {
 
     req.session.data["validationAppeals"][objIndex].status = req.session.data["validation-"+v+"-outcome"];
 
+    if (req.session.data["validation-"+v+"-outcome"] == "Something is missing or wrong"){
+
+      reasonsWhy = req.session.data["validation-"+v+"-wrong-reasons"];
+      if (Array.isArray(reasonsWhy)){
+        reasonsWhy.forEach( item => {
+          req.session.data["validationAppeals"][objIndex].reasonsIncomplete.push(item);
+        });
+      } else {
+        req.session.data["validationAppeals"][objIndex].reasonsIncomplete.push(reasonsWhy);
+      }
+
+      reasonsWhyMissing = req.session.data["validation-"+v+"-wrong-reasons-missing"];
+      if (Array.isArray(reasonsWhyMissing)){
+        reasonsWhyMissing.forEach( item => {
+          req.session.data["validationAppeals"][objIndex].reasonsIncompleteMissing.push(item);
+        });
+      } else {
+        req.session.data["validationAppeals"][objIndex].reasonsIncompleteMissing.push(reasonsWhyMissing);
+      }
+      req.session.data["validationAppeals"][objIndex].reasonsIncompleteOther = req.session.data["validation-"+v+"-wrong-other"];
+
+    }
+
     delete req.session.data["validation-"+v+"-outcome"];
     delete req.session.data["validation-"+v+"-descriptionofdevelopment"];
 
@@ -123,47 +146,21 @@ module.exports = function (router) {
     res.redirect(base+'appeal-review');
   })
 
-
 /*
-  router.post(base+'review-appeal', function (req, res) {
-    if (req.session.data['vo-'+v+'-status'] == "Valid"){
-      res.redirect(base+'valid-details');
-    }
-    if (req.session.data['vo-'+v+'-status'] == "Invalid"){
-      res.redirect(base+'invalid-details');
-    }
-    if (req.session.data['vo-'+v+'-status'] == "Something is missing or wrong"){
-      res.redirect(base+'wrong-details');
-    }
-  })
+  router.post(base+'change/reasons-incomplete', function (req, res) {
 
-  router.post(base+'valid-details', function (req, res) {
-    res.redirect(base+'valid-review');
-  })
-
-  router.post(base+'invalid-details', function (req, res) {
-    res.redirect(base+'invalid-review');
-  })
-
-  router.post(base+'wrong-details', function (req, res) {
-    res.redirect(base+'wrong-review');
-  })
-
-  router.post(base+'wrong-review', function (req, res) {
-    if (req.session.data['vo-'+v+'-wrong-completed'] == "completed"){
-      res.redirect(base+'wrong-complete');
-    } else {
-      res.redirect(base+'wrong-review-error');
-    }
-  })
-
-  router.post(base+'wrong-review-error', function (req, res) {
-    if (req.session.data['vo-'+v+'-wrong-completed'] == "completed"){
-      res.redirect(base+'wrong-complete');
-    } else {
-      res.redirect(base+'wrong-review-error');
-    }
+    res.redirect(base+'appeal-review');
   })
 */
+  router.post(base+'appeal-change-outcome', function (req, res) {
+
+    if (req.session.data['validation-'+v+'-outcome'] == "Valid"){
+      res.redirect(base+"outcome-valid");
+    }
+    if (req.session.data['validation-'+v+'-outcome'] == "Invalid"){
+      res.redirect(base+'outcome-invalid');
+    }
+
+  })
   
 }
